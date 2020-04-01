a21e51f11c1814214e95032191c7aaeaf1df2347
TakeThatkinson <francisdatkinson@gmail.com>
2020-04-01 17:20:36 +0100

    set up the 2 states for the checkbox, couldn't toggle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.scss';
+
 import Button from './components/Button.js';
 import TextInput from './components/TextInput';
 import CheckboxInput from './components/CheckboxInput';
 
-function App() {
-  return (
-    <div className="App">
-			<div className='Card'>
-				<img src={logo} className='Logo' alt='logo' />
-				<h1>Example login screen</h1>
-				<h2>Getting started with Green.</h2>
-				<form>
-					<TextInput label='Email Address'></TextInput>
-					<CheckboxInput label='Remember this device' isChecked={false}></CheckboxInput>
-					<Button label='Sign In'></Button>
-				</form>				
-			</div>			
-    </div>
-  );
-}
+export default class App extends React.Component {
+	
+	state = {
+		rememberDevice: true
+	}
 
-export default App;
+	handleClick = () => { // I had trouble preventing the click event from being fired on each render, causing an infinite loop
+		// the two checkbox states do work however, if you change the prop manually
+		console.log("click");
+		this.setState({ rememberDevice: !this.state.rememberDevice });
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<div className='Card'>
+					<img src={logo} className='Logo' alt='logo' />
+					<h1>Example login screen</h1>
+					<h2>Getting started with Green.</h2>
+					<form>
+						{/* custom form components to match the design from the image */}
+						<TextInput label='Email Address'></TextInput>
+						<CheckboxInput label='Remember this device' isChecked={this.state.rememberDevice} /* onClick={this.handleClick()}*/ />
+						<Button label='Sign In'></Button>
+					</form>				
+				</div>			
+			</div>
+		);
+	}
+}
--- src/components/CheckboxInput.js
+++ src/components/CheckboxInput.js
@@ -2,38 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../App.scss';
 
+class CheckboxInput extends React.Component {
+	render() {
+		return (
+			<div className='Input CheckboxInput' >
+				<span className={`Checkbox ${this.props.isChecked ? 'checked' : ''}`}></span>
+				<p className='Label'>{this.props.label}</p>
+			</div>
+		);
+	}
+}
+
 CheckboxInput.propTypes = {
 	label: PropTypes.string,
 	isChecked: PropTypes.bool
 }
-
-function CheckboxInput(props) {
-
-	return (
-		<div className='Input CheckboxInput'>
-			<input type='checkbox'></input>
-			<span className={`Checkbox ${props.isChecked ? 'checked' : ''}`}></span>
-			<p className='Label'>{props.label}</p>
-		</div>
-	);
-}
-
-// class CheckboxInput extends React.Component {
-// 	constructor(props) {
-// 		super(props);
-// 		this.state = {
-// 			isShow: true,
-// 		};
-// 	}
-// 	render() {
-// 		return (
-// 			<div className='Input CheckboxInput'>
-// 			<input type='checkbox'></input>
-// 			<span className='Checkmark' isChecked={props.isChecked}></span>
-// 			<p className='Label'>{props.label}</p>
-// 		</div>
-// 		);
-// 	}
-// }
   
 export default CheckboxInput;
